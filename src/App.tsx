6711df773b49345b1cc2713759db5c7c09881dbd
uroscvetkovic <uroscvetkovic5@gmail.com>
2025-12-09 01:49:03 +0100

    add sticky instructions and reset button

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,102 @@
+import { useRef, useState } from "react";
 import "./App.scss";
-import VideoPreview from "./components/videoPreview/VideoPreview";
 
 function App() {
+  const [videoPreviewText, setVideoPreviewText] = useState("Video Preview");
+  const [showResetButton, setshowResetButton] = useState(false);
+
+  const videoRef = useRef<HTMLVideoElement>(null);
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  const start = async () => {
+    navigator.mediaDevices
+      .getUserMedia({ video: true })
+      .then((stream) => {
+        if (videoRef.current) {
+          videoRef.current.srcObject = stream;
+
+          setTimeout(() => {
+            captureImage();
+          }, 5000);
+        }
+      })
+      .catch((error) =>
+        setVideoPreviewText("Error accessing media devices: " + error)
+      );
+  };
+
+  const captureImage = () => {
+    const video = videoRef.current;
+    const canvas = canvasRef.current;
+
+    if (!video || !canvas) return;
+
+    const ctx = canvas.getContext("2d");
+    if (!ctx) return;
+
+    canvas.width = video.videoWidth;
+    canvas.height = video.videoHeight;
+
+    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+    const stream = video.srcObject as MediaStream;
+    stream.getTracks().forEach((track) => track.stop());
+
+    video.srcObject = null;
+    setshowResetButton(true);
+  };
+
+  const reset = () => {
+    if (videoRef.current && videoRef.current.srcObject) {
+      const stream = videoRef.current.srcObject as MediaStream;
+      stream.getTracks().forEach((track) => track.stop());
+      videoRef.current.srcObject = null;
+    }
+
+    const canvas = canvasRef.current;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      if (ctx) ctx.clearRect(0, 0, canvas.width, canvas.height);
+    }
+
+    setshowResetButton(false);
+  };
+
   return (
     <>
-      <h1 className="title">Video capture</h1>
-      <p className="description">
-        Click the button to allow camera access.
-        <br />A photo will be taken automatically after a few seconds.
-      </p>
+      <div className="instructions">
+        <h1 className="title">Video capture</h1>
+        <p className="description">
+          Click the button to allow camera access.
+          <br />A photo will be taken automatically after a few seconds.
+        </p>
 
-      <VideoPreview />
+        <div className="button-container">
+          <button className="start-button" onClick={() => start()}>
+            Start
+          </button>
+          <button className="reset-button" style={{display: showResetButton ? "block" : "none" }} onClick={() => reset()}>
+            Reset
+          </button>
+        </div>
+      </div>
+
+      <div className="video-preview-root">
+        <div className="video-overlay">
+          <p className="overlay-text">{videoPreviewText}</p>
+          <video
+            className="video-element"
+            ref={videoRef}
+            autoPlay
+            muted
+          ></video>
+        </div>
+
+        <div className="snapshot-overlay">
+          <p className="overlay-text">Snapshot</p>
+          <canvas className="snapshot-canvas" ref={canvasRef}></canvas>
+        </div>
+      </div>
     </>
   );
 }
